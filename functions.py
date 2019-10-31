13590c3f2d307c0a94ed18ec1f72e156821d8390
Andrew Chen <aandrewc@ucla.edu>
2019-10-31 01:46:03 -0700

    drive_towards function is mostly complete; implementation details may need to be changed

--- functions.py
+++ functions.py
@@ -1,14 +1,27 @@
-# Function Defintions 
+import numpy as np
 
+# Function Defintions 
 # map origin: bottom left corner
 # state s = (x, y, h)
 # 	units = (mm, mm, rad)
 
+
+# Global Variables
 map_width = 1000.0  #mm
 map_length = 1000.0  #mm
 
+robot_width = 90.0  #mm
+wheel_radius = 25.0  #mm
+
 max_rpm = 60  # rpm
+robot_circumference = 2*np.pi*(robot_width/2)
+wheel_circumference = 2*np.pi*wheel_radius
+
+velocity = wheel_circumference * max_rpm * (1/60.0)  # mm/sec = mm/rev * rev/min * min/sec
+angular_speed = velocity * (2*np.pi / robot_circumference)    # rad/sec = mm/sec * rad/mm
+
 precision = 1e-3
+PI = np.pi
 
 
 # Adrian
@@ -22,9 +35,84 @@ def nearest_neighbor(S, s_rand):
 	return s_nearest
 
 
+def angle2time(angle):
+	# Return time required for robot to rotate angle radians
+
+	rotation_time = angle / angular_speed
+	return rotation_time
+
+def align_heading(h_start, h_end):
+	# Return the shortest change in angle needed go from h_start to h_end (clockwise: delta<0; counterclockwise: delta>0)
+	delta = h_end - h_start
+	sign = -1 if delta < 0 else 1
+	if abs(delta) > np.pi:
+		delta = -sign * ( (2*np.pi) - delta )  # rotate opposite direction
+	return delta
+
 # Andrew
 def drive_towards(s_start, s_rand, max_time=1):
 	# Drive from state s_start towards s_rand for max_time seconds
+	x_current = s_start[0]
+	y_current = s_start[1]
+	h_current = s_start[2]
+
+	x_target = s_rand[0]
+	y_target = s_rand[1]
+	h_target = s_rand[2]
+
+	# 1. rotate towards the target
+	# 2. drive towards the target
+	# 3. match the target's heading
+
+	remaining_time = max_time
+
+	# compute the positive angle of a straight line connecting s_start and s_rand
+	h_driving = np.arctan2([y_target - y_current], [x_target - x_current])[0]
+	if h_driving < 0:
+		h_driving += (2*PI)
+
+	# rotate to face target point, taking the shortest path
+	delta1 = align_heading(h_current, h_driving)
+	
+	# consider rotating to setup driving backwards
+	sign = -1 if delta1 < 0 else 1
+	delta1_back = -sign * (PI - abs(delta1))
+	h_driving_back = (h_driving + PI) % (2*PI)
+
+	# compute rotations needed to match the target heading
+	delta2 = align_heading(h_driving, h_target)
+	delta2_back = align_heading(h_driving_back, h_target)
+
+	# check with driving direction results in the smallest total rotation
+	backwards = 1
+	if (abs(delta1) + abs(delta2)) < (abs(delta1_back) + abs(delta2_back)):
+		delta = [delta1, delta2]
+	else:
+		delta = [delta1_back, delta2_back]
+		h_driving = h_driving_back
+		backwards = -1
+
+	# store the rotation times and decrease the remaining time
+	rotate_time1 = angle2time(delta[0])
+	remaining_time -= rotate_time1
+
+	rotate_time2 = angle2time(delta[1])
+	remaining_time -= rotate_time2
+
+	# determine how far the robot can travel within the remaining time
+	move_time = remaining_time
+	max_dist = velocity * move_time
+
+	# if the robot can reach the target in time, drive there; otherwise, drive max_dist
+	target_dist = np.sqrt( (y_target - y_current)**2 + (x_target - x_current)**2 )
+	dist = target_dist if target_dist < max_dist else max_dist
+
+	x_new = x_current + backwards*dist*np.cos(h_driving)
+	y_new = y_current + backwards*dist*np.sin(h_driving)
+	h_new = h_target
+
+	s_new = (x_new, y_new, h_new)
+	action = (rotate_time1, move_time, rotate_time2)
 
 	# action = (rotate_time, move, rotate_time)
 	# 	units = (+/- s) 
@@ -72,7 +160,7 @@ def RRT():
 	obstacles = [ (p1, p2, p3, p4) ]
 
 	while(1):
-		s_rand = rand()
+		s_rand = (x_rand, y_rand, h_rand) 
 
 		s_nearest = nearest_neighbor(S, s_rand)
 
@@ -100,6 +188,9 @@ def RRT_star():
 
 
 if __name__ == "__main__":
-	RRT()
-
-
+	start = (0,0,0)
+	end = (1000,0,0)
+	[state, action] = drive_towards(start, end)
+	print("Target:", end)
+	print("Final:", state)
+	print("Times:", action)
